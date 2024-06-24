baa246e22c2bc5033506ea26208ef3bd9226c79a
akorosov <korosov@gmail.com>
2024-06-24 12:57:40 +0200

    switch 1st script to importable

--- scripts/01_convert_rgps_csv.py
+++ scripts/01_convert_rgps_csv.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
-
 import sys
-
 from pysida.lib import dataframe_from_csv
 
-ifile = str(sys.argv[1])
-ofile = ifile.replace('.csv', '.df')
-print(ofile)
+def run(ifile):
+    ofile = ifile.replace('.csv', '.df')
+    print(ofile)
+    df = dataframe_from_csv(ifile)
+    df.to_pickle(ofile)
 
-df = dataframe_from_csv(ifile)
-df.to_pickle(ofile)
+if __name__ == '__main__':
+    ifile = str(sys.argv[1])
+    run(ifile)
