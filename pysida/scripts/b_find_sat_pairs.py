e222c50021f3bf0961f500f8b37a09598b226286
Anton Korosov <korosov@gmail.com>
2025-08-22 16:12:10 +0200

    Improve GetSatPairs interface

--- pysida/scripts/b_find_sat_pairs.py
+++ pysida/scripts/b_find_sat_pairs.py
@@ -5,29 +5,15 @@ import sys
 import pandas as pd
 import numpy as np
 
-from pysida.lib import get_satellite_pairs, BaseRunner
+from pysida.lib import GetSatPairs, BaseRunner
 
 class Runner(BaseRunner):
-    min_time_diff = 0.5
-    max_time_diff = 7
-    min_size = 100
-    r_min = 0.12
-    a_max = 200e6
-    cores = 4
-
     def __call__(self, ifile, date0, date1):
         ofile = ifile.replace('_LP.df', f'_{date1.strftime("%Y%m%d")}_pairs.npz')
         if self.skip_processing(ofile): return ofile
         df = pd.read_pickle(ifile)
-        pairs = get_satellite_pairs(
-            df, date0, date1,
-            min_time_diff=self.min_time_diff,
-            max_time_diff=self.max_time_diff,
-            min_size=self.min_size,
-            r_min=self.r_min,
-            a_max=self.a_max,
-            cores=self.cores
-        )
+        gsp = GetSatPairs(df, **self.__dict__)
+        pairs = gsp.get_all_pairs(date0, date1)
         print('N pairs', len(pairs), ofile)
         np.savez(ofile, pairs=pairs)
         return ofile
